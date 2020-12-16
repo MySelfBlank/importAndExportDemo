3f6ceed161f612bd504e23372f470a1c0612bcbb
Yzh <yzhyaoxiaozhi@foxmail.com>
2020-12-16 15:51:20 +0800

    修改递归产生的Bug

--- src/main/java/com/yzh/utilts/SDomainPagesTools.java
+++ src/main/java/com/yzh/utilts/SDomainPagesTools.java
@@ -3,6 +3,7 @@ package com.yzh.utilts;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONObject;
 import com.alibaba.fastjson.JSONArray;
+import com.yzh.Index;
 import com.yzh.api.MyApi;
 import com.yzh.userInfo.UserInfo;
 import onegis.psde.psdm.SDomain;
@@ -29,11 +30,10 @@ public class SDomainPagesTools {
      * @param pageNum
      * @param pageSize
      * @param sDomainName
-     * @param pages
      * @param input
      * @param sDomains
      */
-    public static void getPages(int pageNum, int pageSize, String sDomainName, int pages, Scanner input, List<SDomain> sDomains){
+    public static void getPages(int pageNum, int pageSize, String sDomainName, Scanner input, List<SDomain> sDomains){
         params.put("pageNum", pageNum);
         params.put("pageSize", pageSize);
         params.put("names", sDomainName);
@@ -41,41 +41,11 @@ public class SDomainPagesTools {
         String getRespond = HttpUtil.get(MyApi.getDomain1.getValue(), params);
         logger.debug("获取时空域完毕");
         JSONObject jsonData = formatData(getRespond);
-        pages = jsonData.getInt("pages");
+        Index.pages = jsonData.getInt("pages");
         sDomains.clear();
-        sDomains = JSONArray.parseArray(jsonData.getStr("list"), SDomain.class);
+        sDomains.addAll(JSONArray.parseArray(jsonData.getStr("list"), SDomain.class));
         for (int i = 0; i < sDomains.size(); i++) {
             System.out.println("[" + (i + 1) + "]" + sDomains.get(i).getName());
-        }
-        //是否选择下一页
-        if(pages>pageNum){
-            System.out.println("是否选择下一页？[y/n]");
-            if(input.next().equals("y")){
-                //重新做分页请求
-                pageNum++;
-                getPages(pageNum,pageSize,sDomainName,pages,input,sDomains);
-            }
-        }
-        //是否选择上一页
-        if(pageNum>1&&pageNum<=pages){
-            System.out.println("是否选择上一页？[y/n]");
-            if(input.next().equals("y")){
-                //重新做分页请求
-                pageNum--;
-                getPages(pageNum,pageSize,sDomainName,pages,input,sDomains);
-            }
-        }
-        System.out.println();
-        System.out.println("请选择时空域前的序号");
-        try {
-            Integer i = input.nextInt() - 1;
-            while (i > sDomains.size()) {
-                System.out.println("输入无效请重新输入：");
-                i = input.nextInt() - 1;
-            }
-            UserInfo.domain = sDomains.get(i).getId().toString();
-        } catch (Exception e) {
-            e.getMessage();
         }
     }
 }
